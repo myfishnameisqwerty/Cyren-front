81735ceb6d23f8003d793d714bad3de399d89b2c
myfishnameisqwerty <v_jenya@yahoo.com>
2021-05-02 11:08:03 +0300

    task is done

--- src/components/displayUsers/DisplayUsers.jsx
+++ src/components/displayUsers/DisplayUsers.jsx
@@ -1,21 +1,46 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { fetchUsers } from '../../redux'
-import {Link} from 'react-router-dom'
-import { Button } from '@material-ui/core'
-import EditIcon from '@material-ui/icons/Edit';
-import LockIcon from '@material-ui/icons/Lock';
-import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
-import './displayUsers.css'
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../../redux";
+import { NavLink, Link } from "react-router-dom";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+import LockIcon from "@material-ui/icons/Lock";
+import AddIcon from "@material-ui/icons/Add";
+import DeleteIcon from "@material-ui/icons/Delete";
+import UpdateIcon from "@material-ui/icons/Update";
+import "./displayUsers.css";
+import axios from "axios";
 
-function DisplayUsers ({ userData, fetchUsers }) {
-  const columns = [ 'Name', 'Email', 'Dragons', 'Actions' ]
+function DisplayUsers({ userData, fetchUsers }) {
+  const columns = ["Select", "Name", "Email", "Dragons", "Edit"];
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
+  const [open, setOpen] = useState(false);
+  const [userToDelete, setUserToDelete] = useState([]);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-  
+  const handleClose = () => {
+    setOpen(false);
+  };
+  function deleteRequest() {
+    userToDelete.forEach((id) => {
+      axios
+        .delete(`${process.env.REACT_APP_SERVER_ADDRESS}/users/${id}`)
+        .then(() => fetchUsers());
+    });
+
+    handleClose();
+  }
 
   return userData.loading ? (
     <h2>Loading</h2>
@@ -24,65 +49,112 @@ function DisplayUsers ({ userData, fetchUsers }) {
   ) : (
     <div>
       <h2>My users</h2>
-      
+
       <div>
-      
-        {userData &&
-          userData.users &&
+        {userData && userData.users && (
           <table>
             <thead>
               <tr>
-              <Button variant="contained" id='menuButton'><AddIcon/>Create user</Button>
+                <td>
+                  <NavLink to={`/create/`}>
+                    <Button variant="contained" className="menuButton">
+                      <AddIcon />
+                      Create user
+                    </Button>
+                  </NavLink>
+                </td>
+
+                <td>
+                  <div onClick={handleClickOpen} className="menuButton">
+                    <DeleteIcon />
+                  </div>
+                </td>
+                <td>
+                  <div onClick={fetchUsers} className="menuButton">
+                    <UpdateIcon />
+                  </div>
+                </td>
               </tr>
               <tr>
-                {columns.map((header, i) => <th key={i}>{header}</th>)}
+                {columns.map((header, i) => (
+                  <th key={i}>{header}</th>
+                ))}
               </tr>
             </thead>
             <tbody>
-              
-                {userData.users.map(user => 
+              {userData.users.map((user, i) => (
                 <tr key={user.id}>
+                  <td>
+                    <input
+                      type="checkbox"
+                      value={user.id}
+                      disabled={user.fetched}
+                      onClick={(e) => {
+                        let users = [...userToDelete];
+                        if (e.target.checked) users.push(e.target.value);
+                        else
+                          users = users.filter(
+                            (id) => id !== e.target.value.id
+                          );
+                        setUserToDelete(users);
+                      }}
+                    />
+                  </td>
                   <td>{user.name}</td>
                   <td>{user.email}</td>
                   <td>{user.dragons}</td>
-                  <td>{user.fetched?<LockIcon/>:
-                  <React.Fragment>
-                    <Link to={`/users/${user.id}`}>
-                    <EditIcon/>
-                  </Link>
-                  
-                  <Link to={`/users/${user.id}`}>
-                    <DeleteIcon/>
-                  </Link>
-                  </React.Fragment>
-                  
-                   }</td>
+                  <td>
+                    {user.fetched ? (
+                      <LockIcon />
+                    ) : (
+                      <Link to={`/users/${user.id}`}>
+                        <EditIcon />
+                      </Link>
+                    )}
+                  </td>
                 </tr>
-                )}
-              
-
+              ))}
             </tbody>
           </table>
-        }
-          
+        )}
       </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Plese confirm your action"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you shure, that you want to delete this user?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            No
+          </Button>
+          <Button onClick={() => deleteRequest()} color="primary" autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
-  )
+  );
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    userData: state.user
-  }
-}
+    userData: state.user,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    fetchUsers: () => dispatch(fetchUsers())
-  }
-}
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(DisplayUsers)
+export default connect(mapStateToProps, mapDispatchToProps)(DisplayUsers);
--- src/components/editUser/EditUser.jsx
+++ src/components/editUser/EditUser.jsx
@@ -1,65 +1,114 @@
-import axios from 'axios'
-import React, {useState, useEffect, useRef} from 'react'
-import { Button } from '@material-ui/core'
-import '../form.css'
-
+import axios from "axios";
+import React, { useState, useEffect, useRef } from "react";
+import { Button } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+import "../form.css";
+import HomeIcon from "@material-ui/icons/Home";
 export default function EditUser(props) {
-    const [user, setUser] = useState(null)
-    const [validName, setValidName] = useState(null)
-    const [nameError, setNameError] = useState(null)
-    const nameRef = useRef(null)
-    const dragonsRef = useRef(null)
-    function checkName(){
-        setValidName('invalid')
-        setNameError("Name should contain only the characters: a-z ,.'-")
-        if (nameRef.current.value && /^([a-zA-Z\s\.'é-]+)$/.test(nameRef.current.value)){
-            setValidName('valid')
-            setNameError('')
-        }
+  const [user, setUser] = useState(null);
+  const [validName, setValidName] = useState(null);
+  const [nameError, setNameError] = useState(null);
+  const nameRef = useRef(null);
+  const dragonsRef = useRef(null);
+  function checkName() {
+    setValidName("invalid");
+    setNameError("Name should contain only the characters: a-z ,.'-");
+    if (
+      nameRef.current.value &&
+      /^([a-zA-Z\s\.'é-]+)$/.test(nameRef.current.value)
+    ) {
+      setValidName("valid");
+      setNameError("");
     }
-    function getValidationColor(field){
-        if (field==='valid')
-            return 'rgb(162, 236, 144)'
-        else if (field==='invalid')
-            return 'rgb(255, 164, 164)'
-    }
-    function submitUpdate(){
-        let newData = new Object()
-        newData.name = nameRef.current.value
-        newData.dragons = dragonsRef.current.value
-        axios.put(`${process.env.REACT_APP_SERVER_ADDRESS}/users/${props.match.params.id}`, newData)
-        .then(response => props.history.push('/'))
-    }
-    useEffect(() => {
-        axios.get(`${process.env.REACT_APP_SERVER_ADDRESS}/users?filter={"id":"${props.match.params.id}"}`)
-        .then(res => {
-            setUser(res.data[0])})
-    }, [])
-    return ( user &&
-        
-        <div id="container">
-            <h2>Edit user</h2>
-            <table>
-                <tbody>
-                    <tr>
-                        <td><label htmlFor='email'>Email</label></td>
-                        <td><input  name='email' disabled defaultValue={user.email}/></td>
-                    </tr>
-                    <tr>
-                        <td><label htmlFor='name'>Name</label></td>
-                        <td><input  ref={nameRef} name='name' onBlur={checkName} defaultValue={user.name} style={{backgroundColor: getValidationColor(validName)}}/></td>
-                        <td><label>{nameError}</label></td>
-                    </tr>
-                    <tr>
-                        <td><label htmlFor='dragons'>Dragons</label></td>
-                        <td><input type='number' min="0" ref={dragonsRef} name='dragons' defaultValue={user.dragons} /></td>
-                    </tr>
-                </tbody>
-            </table>
-            <Button variant="contained" id="submitButton" onClick={submitUpdate}>Edit</Button> 
-            
-        </div>
-    )
-}
+  }
+  function getValidationColor(field) {
+    if (field === "valid") return "rgb(162, 236, 144)";
+    else if (field === "invalid") return "rgb(255, 164, 164)";
+  }
+  function submitUpdate() {
+    if(!nameError && dragonsRef.current.value>=0){
+        let newData = new Object();
+        newData.name = nameRef.current.value;
+        newData.dragons = dragonsRef.current.value;
+        axios
+          .put(
+            `${process.env.REACT_APP_SERVER_ADDRESS}/users/${props.match.params.id}`,
+            newData
+          )
+          .then((response) => props.history.push("/"));
 
-  
+    }
+  }
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.REACT_APP_SERVER_ADDRESS}/users?filter={"id":"${props.match.params.id}"}`
+      )
+      .then((res) => {
+        console.log(
+          `${process.env.REACT_APP_SERVER_ADDRESS}/users?filter={"id":"${props.match.params.id}"}`
+        );
+        setUser(res.data[0]);
+      });
+  }, []);
+  return (
+    <div>
+      <NavLink to="/">
+        <HomeIcon fontSize="large" />
+      </NavLink>{" "}
+      {user ? (
+        <div id="container">
+          <h2>Edit user</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <label htmlFor="email">Email</label>
+                </td>
+                <td>
+                  <input name="email" disabled defaultValue={user.email} />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label htmlFor="name">Name</label>
+                </td>
+                <td>
+                  <input
+                    ref={nameRef}
+                    name="name"
+                    onBlur={checkName}
+                    defaultValue={user.name}
+                    style={{ backgroundColor: getValidationColor(validName) }}
+                  />
+                </td>
+                <td>
+                  <label>{nameError}</label>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label htmlFor="dragons">Dragons</label>
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    ref={dragonsRef}
+                    name="dragons"
+                    min="0"
+                    defaultValue={user.dragons}
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <Button variant="contained" id="submitButton" onClick={submitUpdate}>
+            Edit
+          </Button>
+        </div>
+      ) : (
+        <p>loading...</p>
+      )}
+    </div>
+  );
+}
